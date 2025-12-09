79d5ad1a35aef62cf81493847ef9dd8a1df005fe
ssktgm <51650332+ssktgm@users.noreply.github.com>
2025-12-09 23:45:33 +0000

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ants-dashboard/",
+  base: "/ants_dashboard/",
 })
